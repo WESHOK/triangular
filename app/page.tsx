c90c68082c98ce981abfc2de4bf86e99b37e6270
WESHOK <113446901+WESHOK@users.noreply.github.com>
2024-01-25 14:32:56 +0300

    modified:   app/layout.tsx 	modified:   app/page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,17 @@
+'use client'
+
+import { Fragment } from "react";
+
+let a = 10;
+function increaseA()
+{
+    a++;
+}
+
 export default function Home()
 {
-    return <div>HELLO!</div>;
+    return <Fragment>
+        <button onClick={() => increaseA}>УВЕЛИЧИТЬ</button>
+        <div style={{ width: '100px', height: `${a}px` }}></div>
+    </Fragment>;
 }
